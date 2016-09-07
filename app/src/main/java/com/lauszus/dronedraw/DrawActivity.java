dcd40fb0be5a1ba5f206e55b45550c48ab70e525
Kristian Sloth Lauszus <lauszus@gmail.com>
2016-09-07 00:45:24 -0500

    Removed some unused code

--- app/src/main/java/com/lauszus/dronedraw/DrawActivity.java
+++ app/src/main/java/com/lauszus/dronedraw/DrawActivity.java
@@ -22,8 +22,6 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class DrawActivity extends AppCompatActivity {
@@ -35,9 +33,6 @@ public class DrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_draw);
 
         final DrawingView mDrawView = (DrawingView) findViewById(R.id.draw_view);
@@ -46,6 +41,7 @@ public class DrawActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         Button mClearButton = (Button) findViewById(R.id.clear_button);
